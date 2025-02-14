69d7ba534c4f86ae716fb203805a381f5feb57ed
Laura <versaillestgz@gmail.com>
2025-02-14 13:40:39 -0300

    feat: cria os métodos sacar, depositar e visualizar na classe Conta e instancia o objeto c1 na classe Menu

--- conta/src/conta/Menu.java
+++ conta/src/conta/Menu.java
@@ -2,10 +2,18 @@ package conta;
 
 import java.util.Scanner;
 import conta.util.Cores;
+import conta.model.Conta;
 
 public class Menu {
 
 	public static void main(String[] args) {
+		
+		Conta c1 = new Conta(1, 123, 1, "Adriana", 10000.0f);
+		c1.visualizar();
+		c1.sacar(12000.0f);
+		c1.visualizar();
+		c1.depositar(5000.0f);
+		c1.visualizar();
 	
 		Scanner sc = new Scanner(System.in);
 		
