9df8e6e8add99f1ff165e48f8cf0f39780fb8d6c
Laura <versaillestgz@gmail.com>
2025-02-14 14:02:25 -0300

    feat: adiciona os métodos sacar e visualizar à classe ContaCorrente

--- conta/src/conta/model/ContaCorrente.java
+++ conta/src/conta/model/ContaCorrente.java
@@ -16,5 +16,30 @@ public class ContaCorrente extends Conta{
 	public void setLimite(float limite) {
 		this.limite = limite;
 	}
+	
+	/** Realiza um saque na conta, considerando o limite de crédito e o saldo disponível.
+	 * @param valor: quantia a ser sacada
+	 * @return true se a operação for bem sucedida;
+	 * ou false se o saldo+limite for insuficiente para completar a operação.
+	 */
+	@Override
+	public boolean sacar(float valor) { 
+		
+		if(this.getSaldo() + this.getLimite() < valor) {
+			System.out.println("\n Saldo Insuficiente!");
+			return false;
+		}
+		
+		this.setSaldo(this.getSaldo() - valor);
+		return true;
+		
+	}
+	
+	/**Exibe os dados da conta e o limite de crédito.*/
+    @Override
+	public void visualizar() {
+		super.visualizar();
+		System.out.println("Limite de Crédito: " + this.limite);
+	}
 
 }
